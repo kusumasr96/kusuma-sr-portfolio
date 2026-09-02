a20d388206f0242a367af85b73cdc953ed3ba29f
kusumasr96 <kusumasr926@gmail.com>
2026-09-02 14:50:46 +0530

    Update server.d.ts

--- src/convex/_generated/server.d.ts
+++ src/convex/_generated/server.d.ts
@@ -1,1 +1,14 @@
+import { GenericActionCtx, GenericMutationCtx, GenericQueryCtx } from "convex/server";
+
+export declare const query: any;
+export declare const mutation: any;
+export declare const action: any;
+export declare const internalQuery: any;
+export declare const internalMutation: any;
+export declare const internalAction: any;
+
+export type QueryCtx = GenericQueryCtx<any>;
+export type MutationCtx = GenericMutationCtx<any>;
+export type ActionCtx = GenericActionCtx<any>;
+
 export * from "convex/server";
